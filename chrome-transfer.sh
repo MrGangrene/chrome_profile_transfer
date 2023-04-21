d4c6c4709eb9c6f6e32b2ca3b3f9ed2ac2f28419
MrGangrene <130441359+MrGangrene@users.noreply.github.com>
2023-04-21 17:24:56 +0200

    echo cleanup

--- chrome-transfer.sh
+++ chrome-transfer.sh
@@ -45,8 +45,9 @@ elif [ $(echo -e "$new_profile_dir" | wc -l) -gt 2 ]; then
   done
 fi
 
-echo This is the new folder: ---"$new_profile_dir"---
-echo -e This is the old folder ==="$old_profile_dir"=== \n
+echo This is the new folder: ==="$new_profile_dir"===
+echo -e This is the old folder ==="$old_profile_dir"===
+echo ""
 echo "========================================================================================================================================="
 echo First we are going to create a backup of both folders. juuuuust in case we break it.....
 echo You can find a backup tarball of both the old profile folder and the new profile folder on the Desktop in a folder called "Chrome Backup"
